8d813eaaa4f39d6cf0b6ab86880ead8c7a3a41df
XiaoYi33 <1029807850@qq.com>
2024-02-20 23:20:24 +0800

    不使用

--- springboot/src/test/java/com/example/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.springboot;
 
-import com.example.springboot.utils.MD5PasswordEncoder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
