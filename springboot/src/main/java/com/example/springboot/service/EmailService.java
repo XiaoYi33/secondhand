f05f0bdc435aaa49a17ce60dc5b3c7173010704c
XiaoYi33 <1029807850@qq.com>
2024-03-03 17:32:58 +0800

    增加邮件服务

---
+++ springboot/src/main/java/com/example/springboot/service/EmailService.java
@@ -0,0 +1,31 @@
+package com.example.springboot.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class EmailService {
+    @Resource
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+    public void sendEmail(String to,String subject ,String text) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true);
+        mimeMessageHelper.setFrom(from);
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(text);
+        javaMailSender.send(message);
+    }
+
+}
--- springboot/src/test/java/com/example/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -1,15 +1,20 @@
 package com.example.springboot;
 
 import cn.hutool.core.date.LocalDateTimeUtil;
+import com.example.springboot.service.EmailService;
+import jakarta.mail.MessagingException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
 @SpringBootTest
 class SpringbootApplicationTests {
-
+	@Resource
+	private EmailService emailService;
 	@Test
-	void test(){
-
+	void test() throws MessagingException {
+		emailService.sendEmail("2678089155@qq.com","测试","测试测试");
 	}
 	@Test
 	void contextLoads() {
