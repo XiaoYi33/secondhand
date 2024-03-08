35eaacf0a0d1123f107aeb552663879b9e799196
XiaoYi33 <1029807850@qq.com>
2024-03-08 16:49:00 +0800

    修改验证码在邮件中的格式

--- springboot/src/main/java/com/example/springboot/service/UserService.java
+++ springboot/src/main/java/com/example/springboot/service/UserService.java
@@ -101,7 +101,11 @@ public class UserService extends ServiceImpl<UserMapper,User>{
         }
         String code = RandomCode.generateRandomCode();//获取验证码
         String subject = "找回密码验证";
-        String text = "您的验证码是:"+code;
+        String text = "<html><body>" +
+                "<h2>找回密码验证</h2>" +
+                "<p>尊敬的用户，您的验证码是：</p>" +
+                "<h1 style='font-size: 32px; color: #FF0000;'>" + code + "</h1>" +
+                "</body></html>";
         emailService.sendEmail(dbUser.getEmail(),subject,text);
         verificationCodes.put(dbUser.getUsername(),code);
 //        //dbUser.setPassword(MD5PasswordEncoder.encode("123456"));
